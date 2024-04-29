f315059be2969d81ebee702c03233fae7c307d03
Zhekalyy <165782077+Zhekalyy@users.noreply.github.com>
2024-04-29 20:33:21 +0500

    MyMinHeap.java

--- src/MyMinHeap.java
+++ src/MyMinHeap.java
@@ -1,16 +1,42 @@
+/**
+ * MyMinHeap represents a min-heap data structure.
+ * It is implemented using a MyList for storing elements.
+ *
+ * @param <T> the type of elements in the heap, must extend Comparable
+ */
 public class MyMinHeap<T extends Comparable<T>> {
+
     private MyList<T> list;
 
+    /**
+     * Constructs a new MyMinHeap with the specified MyList.
+     * Initializes the heap by building it.
+     *
+     * @param list the MyList to be used as the underlying storage for the heap
+     */
     public MyMinHeap(MyList<T> list) {
         this.list = list;
         buildHeap();
     }
 
+    /**
+     * Inserts a new element into the heap.
+     * Adjusts the heap property by moving the new element up if necessary.
+     *
+     * @param item the element to be inserted into the heap
+     */
     public void insert(T item) {
         list.add(item);
         heapifyUp(list.size() - 1);
     }
 
+    /**
+     * Deletes and returns the minimum element from the heap.
+     * Adjusts the heap property by moving the last element to the root and then heapifying down.
+     *
+     * @return the minimum element from the heap
+     * @throws IllegalStateException if the heap is empty
+     */
     public T deleteMin() {
         if (isEmpty()) {
             throw new IllegalStateException("Heap is empty");
@@ -25,6 +51,12 @@ public class MyMinHeap<T extends Comparable<T>> {
         return min;
     }
 
+    /**
+     * Returns the minimum element from the heap without removing it.
+     *
+     * @return the minimum element from the heap
+     * @throws IllegalStateException if the heap is empty
+     */
     public T findMin() {
         if (isEmpty()) {
             throw new IllegalStateException("Heap is empty");
@@ -32,20 +64,32 @@ public class MyMinHeap<T extends Comparable<T>> {
         return list.get(0);
     }
 
+    /**
+     * Checks if the heap is empty.
+     *
+     * @return true if the heap is empty, false otherwise
+     */
     public boolean isEmpty() {
         return list.size() == 0;
     }
 
+    /**
+     * Returns the number of elements in the heap.
+     *
+     * @return the number of elements in the heap
+     */
     public int size() {
         return list.size();
     }
 
+    // Builds the heap by performing heapify down on all non-leaf nodes
     private void buildHeap() {
         for (int i = (list.size() / 2) - 1; i >= 0; i--) {
             heapifyDown(i);
         }
     }
 
+    // Restores the heap property by moving the element up
     private void heapifyUp(int index) {
         int parentIndex = (index - 1) / 2;
         while (index > 0 && list.get(index).compareTo(list.get(parentIndex)) < 0) {
@@ -55,6 +99,7 @@ public class MyMinHeap<T extends Comparable<T>> {
         }
     }
 
+    // Restores the heap property by moving the element down
     private void heapifyDown(int index) {
         int leftChildIndex = 2 * index + 1;
         int rightChildIndex = 2 * index + 2;
@@ -71,6 +116,7 @@ public class MyMinHeap<T extends Comparable<T>> {
         }
     }
 
+    // Swaps two elements in the list
     private void swap(int i, int j) {
         T temp = list.get(i);
         list.set(i, list.get(j));
